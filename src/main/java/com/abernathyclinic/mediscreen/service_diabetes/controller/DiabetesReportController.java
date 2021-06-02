ea373c752aec6e07ab0bc44545cdc6638f801067
Damien <damiencampagnapro@gmail.com>
2021-06-02 16:43:52 +0200

    Add of logs using Spring Boot AOP

--- src/main/java/com/abernathyclinic/mediscreen/service_diabetes/controller/DiabetesReportController.java
+++ src/main/java/com/abernathyclinic/mediscreen/service_diabetes/controller/DiabetesReportController.java
@@ -1,4 +1,4 @@
-package com.abernathyclinic.mediscreen.service_diabetes;
+package com.abernathyclinic.mediscreen.service_diabetes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
